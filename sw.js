ac21b4c4d83ee82e5a8a25db7a3196681149edfa
Brian DalCorobbo <dalcorob@gmail.com>
2018-08-20 21:54:43 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,56 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var staticCacheName = 'restaurant-reviews-v2';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(caches.open(staticCacheName).then(function (cache) {
+    return cache.addAll([
+    	'./index.html',
+    	'./css/styles.css',
+    	'./data/restaurants.json',
+    	'./js/dbhelper.js',
+      './js/main.js',
+      './js/restaurant_info.js',
+      './img/1.jpg',
+      './img/2.jpg',
+      './img/3.jpg',
+      './img/4.jpg',
+      './img/5.jpg',
+      './img/6.jpg',
+      './img/7.jpg',
+      './img/8.jpg',
+      './img/9.jpg',
+      './img/10.jpg',
+      'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+      'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
+    	]);
+  	})
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(caches.keys().then(function (cacheNames) {
+    return Promise.all(cacheNames.filter(function (cacheName) {
+      return cacheName.startsWith('restaurant-') && cacheName != staticCacheName;
+    }).map(function (cacheName) {
+      return caches['delete'](cacheName);
+    }));
+  }));
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(caches.match(event.request).then(function (response) {
+    return response || fetch(event.request);
+  }));
+});
+
+},{}],2:[function(require,module,exports){
+"use strict";
+
+var r = FetchEvent.prototype.respondWith;
+FetchEvent.prototype.respondWith = function () {
+  return new URL(this.request.url).search.endsWith("bypass-sw") ? void 0 : r.apply(this, arguments);
+};
+
+},{}]},{},[1,2])

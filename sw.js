bdada58f0f61e3703ff2cf0482b2cbc1cfc20614
Brian DalCorobbo <dalcorob@gmail.com>
2018-08-24 15:09:15 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,57 +1,61 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+var staticCacheName = 'restaurant-reviews-v3';
 
-var staticCacheName = 'restaurant-reviews-v2';
-
-self.addEventListener('install', function (event) {
-  event.waitUntil(caches.open(staticCacheName).then(function (cache) {
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(staticCacheName).then(function(cache) {
     return cache.addAll([
-    	'./index.html',
-      './restaurant.html',
-    	'./css/styles.css',
-    	'./data/restaurants.json',
-    	'./js/dbhelper.js',
-      './js/main.js',
-      './js/restaurant_info.js',
-      './img/1.jpg',
-      './img/2.jpg',
-      './img/3.jpg',
-      './img/4.jpg',
-      './img/5.jpg',
-      './img/6.jpg',
-      './img/7.jpg',
-      './img/8.jpg',
-      './img/9.jpg',
-      './img/10.jpg',
+      '/',
+    	'index.html',
+      'restaurant.html',
+    	'css/styles.css',
+    	'data/restaurants.json',
+    	'js/dbhelper.js',
+      'js/main.js',
+      'js/restaurant_info.js',
+      'img/1.jpg',
+      'img/2.jpg',
+      'img/3.jpg',
+      'img/4.jpg',
+      'img/5.jpg',
+      'img/6.jpg',
+      'img/7.jpg',
+      'img/8.jpg',
+      'img/9.jpg',
+      'img/10.jpg',
       'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-      'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
+      'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.css'
     	]);
   	})
   );
 });
 
-self.addEventListener('activate', function (event) {
-  event.waitUntil(caches.keys().then(function (cacheNames) {
-    return Promise.all(cacheNames.filter(function (cacheName) {
-      return cacheName.startsWith('restaurant-') && cacheName != staticCacheName;
-    }).map(function (cacheName) {
-      return caches['delete'](cacheName);
+self.addEventListener('activate', function(event) {
+  event.waitUntil(caches.keys().then(function(cacheNames) {
+    return Promise.all(cacheNames.filter(function(cacheName) {
+      return cacheName.startsWith('restaurant-reviews-v') && cacheName != staticCacheName;
+    }).map(function(cacheName) {
+      return caches.delete(cacheName);
     }));
   }));
 });
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request).then(function (response) {
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/'));
+      return;
+    }
+  }
+
+  event.respondWith(caches.match(event.request).then(function(response) {
     return response || fetch(event.request);
   }));
 });
 
-},{}],2:[function(require,module,exports){
-"use strict";
-
-var r = FetchEvent.prototype.respondWith;
-FetchEvent.prototype.respondWith = function () {
-  return new URL(this.request.url).search.endsWith("bypass-sw") ? void 0 : r.apply(this, arguments);
-};
-
-},{}]},{},[1,2])
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
